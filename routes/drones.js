4297726e88ad237053bd9b8b15df614372db930a
Nash Zangio <nash.c.zangio@gmail.com>
2022-08-13 15:58:59 +0200

    first commit

--- routes/drones.js
+++ routes/drones.js
@@ -2,25 +2,41 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const droneModel = require("../models/Drone.model.js");
+
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
-});
+  droneModel.find()
+  .then((allDrones) => {
+    console.log(allDrones);
+    res.render(`drones/list.hbs`, { allDrones });
+})});
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+    res.render("drones/create-form.hbs");
+  });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+const { name, propeller, maxSpeed } = req.body;
+
+  droneModel.create(req.body)
+  .then((newDrone)=>{res.redirect("/drones")})
+  .catch((error) => `Error while creating a new drone: ${error}`);
 });
+  
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const droneId = req.params.id
+  console.log(req.params.id)
+  DroneModel.findById(droneId)
+  .then((foundDrone)=> {
+    console.log(foundDrone)
+  })
+  .catch((error)=> `Error while updating drones: ${error}`)
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+
+
+
+const mongoose = require('mongoose');
+const droneModel = require('../models/Drone.model');
+
+
+require("../db");
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+
+
+
+  droneModel.insertMany(drones)
+        .then((drones) =>{
+            drones.forEach((drones) => console.log(drones.name))
+        mongoose.connection.close();
+        })
+        .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+        mongoose.connection.close()
+
+
+
+  
